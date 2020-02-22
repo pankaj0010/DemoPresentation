3d47558318f251095bc9a27b7b670fd0659ce21b
Pankaj Kumar <pank@microsoft.com>
2020-02-22 16:54:51 +0530

    Add add and subtract operations

---
+++ src/operations.cpp
@@ -0,0 +1,9 @@
+#include "operations.h"
+
+public int add (int a, int b) {
+	return a+b;
+}
+
+public int subtract (int a, int b) {
+	return a-b;
+}
---
+++ src/operations.h
@@ -0,0 +1,3 @@
+#include <iostream>
+public int add(int a, int b);
+public int subtract(int a, int b);
